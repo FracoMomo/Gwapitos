700bf15340daeda84d1606a4ce5b8695cc5d2438
Monique Camoro <lolcool8000.camoro@gmail.com>
2022-12-11 15:16:43 +0800

    initial commit

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var menuBtn = document.getElementById("menuBtn");
 var sideNav = document.getElementById("sideNav");
 
-sideNav.style.left = "-280px";
+sideNav.style.left = "-290px";
 menuBtn.onclick = function(){
-    if(sideNav.style.left == "-280px"){
+    if(sideNav.style.left == "-290px"){
         sideNav.style.left = "0";
     }
     else{
-        sideNav.style.left = "-280px";
+        sideNav.style.left = "-290px";
     }
 }
